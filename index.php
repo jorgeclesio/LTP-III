84f53866cce9c4d9fba0e0869b36f49454e82a0d
Jorge Clésio <jorgepebas@gmail.com>
2019-02-26 16:46:39 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<meta charset="UTF-8">
+<?php
+require 'carro.php';
+echo "<pre>";
+
+
+$Mobi = new Carro();
+$Pop = new Moto();
+
+
+echo $Pop->display_moto();
+
+//Atividade para Próxima Aula
+//Identifiquem 2 objetos do mundo real e implemente suas classes com atributos e métodos com seus modificadores de acessos.
