d5d36fd1d9fe22d25f2cd85a11c9bb1efd47f4e2
Jorge Clésio <jorgepebas@gmail.com>
2019-03-01 23:08:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -11,4 +11,5 @@ $Pop = new Moto();
 echo $Pop->display_moto();
 
 //Atividade para Próxima Aula
+
 //Identifiquem 2 objetos do mundo real e implemente suas classes com atributos e métodos com seus modificadores de acessos.
