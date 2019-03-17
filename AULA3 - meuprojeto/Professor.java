a2a452a26bb4e6841dc0da66fd2b6e6434be0daf
Jorge Clésio <jorgepebas@gmail.com>
2019-03-17 19:00:18 -0300

    Update Professor.java

--- AULA3 - meuprojeto/Professor.java
+++ AULA3 - meuprojeto/Professor.java
@@ -3,6 +3,13 @@ public class Professor extends Pessoa {
 	private String titulacao;
 	private boolean ministrandoAula;
   
+  
+	public boolean getMinistrandoAula() { //Método GET
+		return ministrandoAula;
+	}
+	public void setMinistrandoAula(boolean ministrandoAula) { //Método SET
+		this.ministrandoAula = ministrandoAula;
+	}
 
 
 }
