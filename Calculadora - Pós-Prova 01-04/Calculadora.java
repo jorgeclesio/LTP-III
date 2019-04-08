b1d95c40556c37dc0d9f087e525471d11f39b2de
Jorge Clésio <jorgepebas@gmail.com>
2019-04-08 12:46:39 -0300

    Add files via upload

---
+++ Calculadora - Pós-Prova 01-04/Calculadora.java
@@ -0,0 +1,30 @@
+public class Calculadora {
+
+	private double num1 = 0;
+	private double num2 = 0;
+	
+	
+	public double somar(double num1, double num2){
+		System.out.print (num1 + num2);
+		return 0;
+	}
+	
+	public double subtrair(double num1, double num2){
+		System.out.print ( num1 - num2);
+		return 0;
+	}
+	
+	
+	public double dividir(double num1, double num2){
+		System.out.print ( num1 / num2);
+		return 0;
+	}
+	
+	public double multiplicar(double num1, double num2){
+		System.out.print ( num1 * num2);
+		return 0;
+	}
+	
+	
+}
+
---
+++ Calculadora - Pós-Prova 01-04/main.java
@@ -0,0 +1,11 @@
+
+public class main {
+	public static void main(String[] args) {
+	
+		Calculadora calculadora = new Calculadora();
+		
+		calculadora.subtrair(5, 2);
+		
+	}
+
+}
