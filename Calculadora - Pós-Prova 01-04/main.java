5bea6b33c4781b94f1818dd59cfc3689b145ee1e
Jorge Clésio <jorgepebas@gmail.com>
2019-04-08 12:51:28 -0300

    Update main.java

--- Calculadora - Pós-Prova 01-04/main.java
+++ Calculadora - Pós-Prova 01-04/main.java
@@ -1,11 +1,11 @@
-
 public class main {
 	public static void main(String[] args) {
 	
 		Calculadora calculadora = new Calculadora();
-		
-		calculadora.subtrair(5, 2);
-		
-	}
 
+		calculadora.somar(5, 2);		
+		calculadora.subtrair(5, 2);
+		calculadora.dividir(5, 2);
+		calculadora.multiplicar(5, 2);
+	}
 }
