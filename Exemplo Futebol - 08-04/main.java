d05216af5d81cc4be737995bc6627f9d95419a58
Jorge Clésio <jorgepebas@gmail.com>
2019-04-10 18:12:17 -0300

    Update main.java

--- Exemplo Futebol - 08-04/main.java
+++ Exemplo Futebol - 08-04/main.java
@@ -2,11 +2,11 @@
 public class main {
 
 	public static void main(String[] args) {
-		Time time1 = new Time("Corinthians");  // Time 1 j· est· disponÌvel na memÛria.
-		Time time2 = new Time("Flamengo"); // Time 2 j· est· disponÌvel na memÛria.
+		Time time1 = new Time("Corinthians");  // Time 1 j√° est√° dispon√≠vel na mem√≥ria.
+		Time time2 = new Time("Flamengo"); // Time 2 j√° est√° dispon√≠vel na mem√≥ria.
 
-		Jogo jogo = new Jogo(time1,time2); //Ao instanciar o Jogo È necess·rio 2 times, assim como especifica o construtor.
-		jogo.iniciarJogo();  //Chama o mÈtodo da Classe Jogo.
+		Jogo jogo = new Jogo(time1,time2); //Ao instanciar o Jogo √© necess√°rio 2 times, assim como especifica o construtor da Classe Jogo.
+		jogo.iniciarJogo();  //Chama o m√©todo da Classe Jogo.
 	}
 
 }
