079bc0d517a95a44b05785066f5067c11f34b213
Jorge Clésio <jorgepebas@gmail.com>
2019-04-16 02:20:43 -0300

    Update main.java

--- Atividade Caixa - 15-04-2019 - JAVA/main.java
+++ Atividade Caixa - 15-04-2019 - JAVA/main.java
@@ -2,19 +2,19 @@ public class main {
 
 	public static void main(String[] args) {
 
-				Produto p[] = new Produto[4];
-				p[0]= new Produto("Açúcar", "ABC", 2.50, 10);
-				p[1]= new Produto("Sal", "ABC", 1.50, 10);
-				p[2]= new Produto("Oleo", "ABC", 3.50, 10);
-				p[3]= new Produto("Biscoito SAL", "ABC", 1, 20);
-			
-				// ou
+		//Produto p[] = new Produto[4];
+		//p[0]= new Produto("Açúcar", "ABC", 2.50, 10);
+		//p[1]= new Produto("Sal", "ABC", 1.50, 10);
+		//p[2]= new Produto("Oleo", "ABC", 3.50, 10);
+		//p[3]= new Produto("Biscoito SAL", "ABC", 1, 20);
+
+		// ou
+
+		Produto p1 = Produto("Açúcar", "ABC", 2.50, 10);
+		Produto p2 = Produto("Biscoito SAL", "ABC", 1, 20);
 		
-				Produto p1 = Produto("Açúcar", "ABC", 2.50, 10);
-				Produto p2 = Produto("Biscoito SAL", "ABC", 1, 20);
 		
-		
-		Venda venda = new Venda("08/04", p[3], 10);
+		Venda venda = new Venda("08/04", p2, 10);
 		venda.FinalizarVenda();
 
 	}
