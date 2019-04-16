c77363203f725576e3c131fae0210bb2d7a8268c
Jorge Clésio <jorgepebas@gmail.com>
2019-04-16 02:19:11 -0300

    Update main.java

--- Atividade Caixa - 15-04-2019 - JAVA/main.java
+++ Atividade Caixa - 15-04-2019 - JAVA/main.java
@@ -5,20 +5,15 @@ public class main {
 		
 		
 		Produto p[] = new Produto[4];
-				p[0]= new Produto("Açúcar", "ABC", 2.50, 10);
+				p[0]= new Produto("AÃ§Ãºcar", "ABC", 2.50, 10);
 				p[1]= new Produto("Sal", "ABC", 1.50, 10);
 				p[2]= new Produto("Oleo", "ABC", 3.50, 10);
 				p[3]= new Produto("Biscoito SAL", "ABC", 1, 20);
 			
-		//p[0].buscar();
 		
 		
 		Venda venda = new Venda("08/04", p[3], 10);
 		venda.FinalizarVenda();
-		
-		
-		//jVenda jvenda = new jVenda();
-		//jvenda.setVisible(true);
-		
+
 	}
 }
