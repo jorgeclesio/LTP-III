2a47e7bc195087d271bfd3c48c4d1bfa90a818a4
Jorge Clésio <jorgepebas@gmail.com>
2019-05-27 19:42:57 -0300

    Add files via upload

---
+++ CRUD-POO-JAVA e PHP/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<head>
+	<meta charset="UTF-8">
+	<title>Cadastrar</title>
+</head>
+<body>
+	<form action="controller/cadastrar_produto.php" method="POST">
+		<input placeholder="Nome" name="nome" required/><Br/>
+		<textarea placeholder="Descrição" name="descricao" required></textarea><Br/>
+		<input placeholder="Marca" name="marca" required/><br/>
+		
+		<input type="submit" name="enviar" value="Cadastrar">
+	</form>
+</body>
+
+<?php 
+require_once '/model/produto.php';
+$produto = new produto();
+$produto->buscar();
+
+
+// $produto = new produto();
+// $produto->cadastrar($_POST['nome'], $_POST['desc'], $_POST['marca']);
+
+
+?>
